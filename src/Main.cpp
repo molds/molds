eb297430a8489359fedb251dd7141c5415870709
Mikiya Fujii <mikiya.fujii@gmail.com>
2012-02-23 17:13:24 +0000

    iostream is added to Main.cc
    git-svn-id: https://svn.sourceforge.jp/svnroot/molds/MolDS/trunk@551 1136aad2-a195-0410-b898-f5ea1d11b9d8

--- src/Main.cpp
+++ src/Main.cpp
@@ -18,6 +18,7 @@
 //************************************************************************//
 #include<stdio.h>
 #include<stdlib.h>
+#include<iostream>
 #include<vector>
 #include<stdexcept>
 #include<boost/shared_ptr.hpp>
